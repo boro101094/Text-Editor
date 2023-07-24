8c73ff77da82c36518c76f81953b96ddb47c680f
Boris Cabrera <boro10194@gmail.com>
2023-07-24 19:45:31 -0400

    Development of the full functionaliy for the App

---
+++ client/src/js/database.js
@@ -0,0 +1,44 @@
+import { openDB } from "idb";
+
+const initdb = async () =>
+  openDB("jate", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
+        return;
+      }
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    },
+  });
+
+export const putDb = async (content) => {
+  // Database and version
+  const jateDB = await openDB("jate", 1);
+  // New transaction specifying db and privileges
+  const tx = jateDB.transaction("jate", "readwrite");
+  // Open desired object store
+  const store = tx.objectStore("jate");
+  // Pass in content
+  const request = store.put({ id: 1, value: content });
+  // Confirmation
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
+
+export const getDb = async () => {
+  // Database and version
+  const jateDB = await openDB("jate", 1);
+  // New transaction specifying db and privileges
+  const tx = jateDB.transaction("jate", "readonly");
+  // Open desired object store
+  const store = tx.objectStore("jate");
+  // Get all request
+  const request = store.getAll();
+  // Confirmation and return
+  const result = await request;
+  console.log("🚀 - data read from database", result);
+  return result.value;
+};
+
+initdb();
